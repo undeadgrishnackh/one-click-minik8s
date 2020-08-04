6e9ce96b0345d27f64f9d8067f7c5f1f8dddb644
Michele Brissoni <michele.brissoni@ibm.com@Micheles-MacBook-Pro-2.local>
2020-08-04 12:31:23 +0200

    Python checker on pre-commit fixed some issues

--- bin/dev/watch.sh
+++ bin/dev/watch.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 ptw tests/ -- --cov-report term --cov=myproj tests/
+
+echo "This is the okd way to run stuff in Python. Please use 'pipenv shell' and then 'pipenv run watch' instead."
