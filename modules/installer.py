37b88ebfb08f18020673c99f8e2e1c1cc287a8dc
Michele Brissoni <michele.brissoni@ibm.com@Micheles-MacBook-Pro-2.local>
2020-08-11 14:00:29 +0200

    [tests] removed the welcome message component

--- modules/installer.py
+++ modules/installer.py
@@ -1,15 +1,16 @@
 from rich.console import Console
 
 from modules.installer_menu import Menu
-from modules.installer_welcome import WelcomeMessage
+
+# from modules.installer_welcome import WelcomeMessage
 
 
 class Installer:
     def __init__(self):
         self.console = Console()
-        self.menu = Menu()
+        # self.menu = Menu()
 
     def start(self):
-        WelcomeMessage().print_welcome_message()
+        # WelcomeMessage().print_welcome_message()
         self.menu.print_the_menu_and_wait_the_answer()
         self.menu.perform_the_action()
